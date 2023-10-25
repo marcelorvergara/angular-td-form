7106efb3638cd78b9a0ab702d4c7a5dd513b1f0f
Marcelo Vergara <marcelorv@gmail.com>
2023-10-25 07:12:35 -0300

    Reset valuse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,5 +54,8 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret
     this.user.answer = this.signupForm.value.questionAnswer
     this.user.gender = this.signupForm.value.gender
+
+    // Reset values
+    this.signupForm.reset()
   }
 }
