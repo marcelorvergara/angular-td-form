972197e74c125a304e349eab27b9373f7028ace2
Marcelo Vergara <marcelorv@gmail.com>
2023-10-25 07:10:30 -0300

    Send values to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   defaultSelectValue = 'pet'
   answer: string = ""
   genders = ['male', 'female']
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -40,5 +48,11 @@ export class AppComponent {
   // With @ViewChild
   onSubmit() {
     console.log('Submited!', this.signupForm)
+    this.submitted = true
+    this.user.username = this.signupForm.value.userData.username
+    this.user.email = this.signupForm.value.userData.email
+    this.user.secretQuestion = this.signupForm.value.secret
+    this.user.answer = this.signupForm.value.questionAnswer
+    this.user.gender = this.signupForm.value.gender
   }
 }
